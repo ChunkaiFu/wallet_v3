1bd7b7b7ce3c44687a621cdeff46dff0a2d9073e
Chunkai <chunkai369@gmail.com>
2023-03-24 00:22:48 -0500

    Last local working app

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,13 +4,18 @@ class RegistrationsController < ApplicationController
     end 
 
     def create 
-        @user=User.new(user_params)
-        if @user.save 
+        @user = User.new(user_params)
+        existing_user = User.find_by(email: @user.email)
+        if existing_user
+            flash[:alert] = "Email address is already in use"
+            render :new
+        elsif @user.save
             session[:user_id] = @user.id
-            redirect_to root_path, notice: "Successfully created account!"
-        else 
-            render :new 
-        end 
+            flash[:notice] = "User created successfully"
+            redirect_to root_path 
+        else
+            render :new
+        end
     end 
 
     private 
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,8 @@ class SessionsController < ApplicationController
   end 
 
   def create 
-    user = User.find_by(email: params[:email])
-    if user.present? && user.authenticate(params[:password])
+    @user = User.find_by(email: params[:email])
+    if @user.present? && @user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to root_path, notice: "Logged in successfully!"
     else 
